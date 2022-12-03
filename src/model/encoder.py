bd5609846ccbd101e8c9aea14bf5ef022cbe2705
ndrwhoang <hoangvuquang1812@gmail.com>
2022-12-03 02:32:37 -0500

    optimize forward pass

--- src/model/encoder.py
+++ src/model/encoder.py
@@ -9,11 +9,11 @@ from transformers import logging as t_logging
 t_logging.set_verbosity_error()
 
 
-class EncoderModule(PreTrainedModel):
+class Reranker(PreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
         self.config = config
-        self.encoder = AutoModel.from_pretrained(self.config.pretrained_name)
+        self.pretrained_encoder = AutoModel.from_pretrained(self.config.pretrained_name)
 
     def _init_weights(self, module):
         # Code from hf itself so this plays nice with the ecosystem/trainer
@@ -35,11 +35,14 @@ class EncoderModule(PreTrainedModel):
     # def _set_gradient_checkpointing(self, module, value=False):
     #     if isinstance(module, AutoModel):
     #         module.gradient_checkpointing = value
-    
+
     def loss_fn(self, scores, labels):
         return F.cross_entropy(scores, labels)
 
     def score_candidates(self, query_embeddings, candidate_embeddings):
+        query_embeddings = F.normalize(query_embeddings, dim=1)
+        candidate_embeddings = F.normalize(candidate_embeddings, dim=1)
+
         return torch.matmul(
             query_embeddings, torch.transpose(candidate_embeddings, 0, 1)
         )
@@ -52,17 +55,23 @@ class EncoderModule(PreTrainedModel):
         candidate_attention_mask,
         labels,
     ):
-        query_out = self.encoder(
-            input_ids=query_ids, attention_mask=query_attention_mask
+        input_ids = torch.cat((query_ids, candidate_ids), dim=0)
+        attention_mask = torch.cat(
+            (query_attention_mask, candidate_attention_mask), dim=0
         )
-        query_embedding = torch.mean(query_out.last_hidden_state, dim=1)
+        encoder_out = self.pretrained_encoder(
+            input_ids=input_ids, attention_mask=attention_mask
+        )
 
-        candidate_out = self.encoder(
-            input_ids=candidate_ids, attention_mask=candidate_attention_mask
-        )
-        candidate_embedding = torch.mean(candidate_out.last_hidden_state, dim=1)
+        query_embedding = encoder_out.last_hidden_state[: query_ids.size(0), :, :]
+        query_embedding = torch.mean(query_embedding, dim=1)
+
+        candidate_embedding = encoder_out.last_hidden_state[query_ids.size(0) :, :, :]
+        candidate_embedding = torch.mean(candidate_embedding, dim=1)
 
         scores = self.score_candidates(query_embedding, candidate_embedding)
         loss = self.loss_fn(scores, labels) if torch.is_tensor(labels) else None
 
-        return SequenceClassifierOutput(logits=scores, loss=loss)
+        return SequenceClassifierOutput(
+            hidden_states=query_embedding, logits=scores, loss=loss
+        )

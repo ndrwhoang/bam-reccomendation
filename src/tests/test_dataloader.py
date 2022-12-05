4a82617bce5bcf3bc96becdabbe0d22c6c0c637b
ndrwhoang <hoangvuquang1812@gmail.com>
2022-12-05 01:31:52 -0500

    more tests

--- src/tests/test_dataloader.py
+++ src/tests/test_dataloader.py
@@ -1,6 +1,7 @@
 import os
 import configparser
 
+import torch
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer
 
@@ -9,7 +10,7 @@ from src.dataset.dataloader import BaseDataset
 
 def test_dataloader(config):
     tokenizer = AutoTokenizer.from_pretrained(config["model"]["pretrained_name"])
-    dataset = BaseDataset(config, tokenizer, "train_subset")
+    dataset = BaseDataset(config, tokenizer, "train")
     dataloader = DataLoader(
         dataset, batch_size=5, shuffle=False, collate_fn=dataset.collate_fn
     )
@@ -20,29 +21,44 @@ def test_dataloader(config):
 
         print(batch["query_ids"].size())
         print(batch["query_attention_mask"].size())
+        print(torch.isnan(batch["query_ids"]).any())
+        print(torch.isnan(batch["query_attention_mask"]).any())
         print(batch["candidate_ids"].size())
         print(batch["candidate_attention_mask"].size())
+        print(torch.isnan(batch["candidate_ids"]).any())
+        print(torch.isnan(batch["candidate_attention_mask"]).any())
         print(batch["labels"].size())
         print(batch["labels"])
+        print(torch.isnan(batch["labels"]).any())
+
+        if (
+            torch.isnan(batch["query_ids"]).any()
+            or torch.isnan(batch["query_attention_mask"]).any()
+            or torch.isnan(batch["candidate_ids"]).any()
+            or torch.isnan(batch["candidate_attention_mask"]).any()
+            or torch.isnan(batch["labels"]).any()
+        ):
+            raise ValueError
         print("============================================")
 
 
 def test_dataloader_decode_sample(config):
-    tokenizer = AutoTokenizer.from_pretrained(config['model']['pretrained_name'])
-    dataset = BaseDataset(config, tokenizer, 'train_subset')
+    tokenizer = AutoTokenizer.from_pretrained(config["model"]["pretrained_name"])
+    dataset = BaseDataset(config, tokenizer, "train_subset")
     dataloader = DataLoader(
         dataset, batch_size=1, shuffle=False, collate_fn=dataset.collate_fn
     )
-    
+
     for i_batch, batch in enumerate(dataloader):
-        query = tokenizer.batch_decode(batch['query_ids'])
+        query = tokenizer.batch_decode(batch["query_ids"])
         print(query[0])
-        print('+++')
-        candidates = tokenizer.batch_decode(batch['candidate_ids'])
+        print("+++")
+        candidates = tokenizer.batch_decode(batch["candidate_ids"])
         for candidate in candidates:
             print(candidate)
-        print('===========-=-=-=-=-=-')
-        print('\n')
+        print("===========-=-=-=-=-=-")
+        print("\n")
+
 
 if __name__ == "__main__":
     print("hello world")
@@ -50,5 +66,5 @@ if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read(os.path.normpath("configs/base_config.ini"))
 
-    # test_dataloader(config)
-    test_dataloader_decode_sample(config)
+    test_dataloader(config)
+    # test_dataloader_decode_sample(config)
